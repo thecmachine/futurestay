0f93e406632c76bfb957b7e29b3cca9aa68b9a6a
Clint Lauby <thecmachine05@gmail.com>
2022-07-20 11:54:49 -0500

    added command

--- app/Console/Commands/GetUsers.php
+++ app/Console/Commands/GetUsers.php
@@ -11,7 +11,7 @@ class GetUsers extends Command
      *
      * @var string
      */
-    protected $signature = 'command:getusers';
+    protected $signature = 'command:getusers {limit=10}';
 
     /**
      * The console command description.
@@ -38,6 +38,37 @@ class GetUsers extends Command
     public function handle()
     {
         $this->info("Get Users Script Begin");
+
+        $limit = $this->argument('limit');
+
+        $service_url     = 'https://randomuser.me/api/';
+        $curl            = curl_init($service_url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_POST, false);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+        $curl_response   = curl_exec($curl);
+        curl_close($curl);
+        $json_response    = json_decode($curl_response);
+        // $quotes          = $json_objekat->contents->quotes;
+        // foreach($quotes as $intKey=>$objQuote){
+        //     echo $objQuote->title       . '<br>';
+        //     echo $objQuote->author      . '<br>';
+        //     echo $objQuote->quote       . '<br>';
+        //     echo $objQuote->background  . '<br>';
+        // }
+
+        $this->info(print_r($json_response->results[0]));
+        $this->newLine(2);
+        $this->info($json_response->results[0]->name->first);
+        $this->info($json_response->results[0]->name->last);
+        $this->info($json_response->results[0]->phone);
+        $this->info($json_response->results[0]->email);
+        $this->info($json_response->results[0]->location->country);
+
+        $this->newLine();
+        $this->info("Limit: " . $limit);
+
+
         return 0;
     }
 }
